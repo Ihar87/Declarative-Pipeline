f463f03160fd6bf9b813f7a89f7aeb53a414d31f
Ihar Hukouski <Ihar_Hukouski@epam.com>
2018-04-10 07:12:12 +0300

    5 commit

--- jenkins/pipeline/ci_syntax_check_test.groovy
+++ jenkins/pipeline/ci_syntax_check_test.groovy
@@ -52,7 +52,7 @@ pipeline {
         script {
           grFiles = findFiles glob: 'ci/jenkins/pipeline/**/*.groovy'
           sh (script: """wget -O jenkins-cli.jar ${env.JENKINS_URL}jnlpJars/jenkins-cli.jar"""
-          withCredentials([usernamePassword(credentialsId: "Jenkins_http_connection", passwordVariable: '12345678', usernameVariable: 'build_runner']) {
+          withCredentials([usernamePassword(credentialsId: "Jenkins_http_connection", passwordVariable: "12345678", usernameVariable: "build_runner"]) {
             for (grFile in grFiles){
                 echo "\n\nChecking ${grFile.name}..."
                 sh (script: """java -jar jenkins-cli.jar -http -auth ${USERNAME}:${TOKEN} -s ${env.JENKINS_URL} declarative-linter < ${WORKSPACE}/${grFile.path}"""
